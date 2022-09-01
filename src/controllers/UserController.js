2f6be897bda6f0ee71e0d059e385b21cd4aeb971
IsaqueMansur <91393713+IsaqueMansur@users.noreply.github.com>
2022-09-01 17:48:04 -0300

    ajustes de segurança de usuário dentro da API - DEV

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -14,9 +14,7 @@ class UserController {
 
   async index(req, res) {
     try {
-      console.log('USER ID', req.userId);
-      console.log('USER EMAIL', req.userEmail);
-      const users = await User.findAll();
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
       return res.json(users);
     } catch (e) {
       return res.json(null);
@@ -26,7 +24,8 @@ class UserController {
   async show(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
-      return res.json(user);
+      const { id, nome, email } = user;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.json(null);
     }
@@ -34,13 +33,6 @@ class UserController {
 
   async update(req, res) {
     try {
-      if (!req.params.id) {
-        return res.status(400).json({
-          errors: ['ID não enviado !'],
-        });
-      }
-      const user = await User.findByPk(req.params.id);
-
       if (!user) {
         return res.status(400).json({
           errors: ['Usuário não existe !'],
@@ -59,12 +51,7 @@ class UserController {
 
   async delete(req, res) {
     try {
-      if (!req.params.id) {
-        return res.status(400).json({
-          errors: ['ID não enviado !'],
-        });
-      }
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.userId);
 
       if (!user) {
         return res.status(400).json({
--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -11,7 +11,6 @@ export default class Aluno extends Model {
       altura: Sequelize.FLOAT,
     }, {
       sequelize,
-
     });
     return this;
   }
